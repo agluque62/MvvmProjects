75ea2ad04794f2f3c246d51e2479f8aa7123cfc7
Arturo Garcia <arturo.garcia@nucleodf.com>
2018-07-24 10:34:34 +0200

    Test de Timers...

---
+++ UnitsTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitsTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("UnitsTestProject")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37752576-079c-4768-8386-d6cefe5fffec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitsTestProject/TimersTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace UnitsTestProject
+{
+    class TimerState
+    {
+        public int Counter;
+    }
+
+    [TestClass]
+    public class TimersTest
+    {
+        private static Timer timer;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var timerState = new TimerState { Counter = 0 };
+
+            timer = new Timer(
+                callback: new TimerCallback((obj) => 
+                {
+                    Trace.WriteLine($"{DateTime.Now:HH:mm:ss.fff}: starting a new callback.");
+                    var state = obj as TimerState;
+                    Interlocked.Increment(ref state.Counter);
+                }),
+                state: timerState,
+                dueTime: 1000,
+                period: 100);
+
+            while (timerState.Counter <= 100)
+            {
+                Task.Delay(100).Wait();
+            }
+
+            timer.Dispose();
+            Trace.WriteLine($"{DateTime.Now:HH:mm:ss.fff}: done.");
+
+            Task.Delay(5000).Wait();
+        }
+    }
+}
